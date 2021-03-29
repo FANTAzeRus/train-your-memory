1491e52ac845a67075bfce3bbd2e949282d89355
Fedor A. Nokhrin <fedor.nohrin@gmail.com>
2021-03-29 11:17:23 +0300

    Подготовка пустый ячеек и заполнение их парными значеними случайным образом

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,66 @@
 <template>
   <div class="game">
-    Игра "Тренируй свою память".
+    <div class="title">Игра "Тренируй свою память".</div>
     <div class="board">
-      <div
-        class="cell"
-        v-for="(cell, idx) in baseConstatnts.cellsCount"
-        :key="`cell_${idx}`"
-      ></div>
+      <div class="cell" v-for="(cell, idx) in state.cells" :key="`cell_${idx}`">
+        {{ cell.value }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import "./styles/index.scss";
-import { baseConstatnts } from "./constants";
+import { baseConstatnts, cellStateConst, gameStateConst } from "./constants";
+import { reactive } from "@vue/reactivity";
+import { onBeforeMount } from "@vue/runtime-core";
 
 export default {
   name: "App",
   components: {},
   setup() {
-    return { baseConstatnts };
+    const state = reactive({
+      cells: [],
+      gameStatus: gameStateConst.stop,
+    });
+    const init = () => {
+      clearCells();
+    };
+    const randomizer = (min, max) => {
+      return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    };
+    const clearCells = () => {
+      state.cells = [];
+      for (let i = 0; i < baseConstatnts.cellsCount; i++) {
+        state.cells.push({
+          id: i,
+          state: cellStateConst.empty,
+          value: 0,
+        });
+      }
+    };
+    const prepareCells = () => {
+      const half = baseConstatnts.cellsCount / 2;
+      for (let i = 0; i < half; i++) {
+        for (let j = 0; j < 2; j++) {
+          let idx = randomizer(0, baseConstatnts.cellsCount - 1);
+
+          if (state.cells[idx].value === 0) {
+            state.cells[idx].state = cellStateConst.set;
+            state.cells[idx].value = i + 1;
+          } else {
+            j--;
+          }
+        }
+      }
+    };
+
+    onBeforeMount(() => {
+      init();
+      prepareCells();
+    });
+
+    return { state };
   },
 };
 </script>
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,17 @@
 export const baseConstatnts = {
-  cellsCount: 24,
+  cellsCount: 12,
+};
+
+export const gameStateConst = {
+  stop: 0,
+  play: 1,
+  levelComplete: 2,
+  gameOver: 3,
+};
+
+export const cellStateConst = {
+  empty: 0,
+  set: 1,
+  success: 2,
+  fail: 3,
 };
